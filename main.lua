a13e5c8fa969e517eaf5fa1a24432dc3d4819f6d
x3sharkx2z <bevz91741@gmail.com>
2025-02-11 19:22:34 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -6269,56 +6269,54 @@ local ClosureBindings = {
             HoverChange = 0.04,
         }
     end,
-	[55] = function()
-        local maui, script, require, getfenv, setfenv = ImportGlobals(55)
-        return {
-            Name = "Reroll Lua",
-            Accent = Color3.fromRGB(173, 216, 230),
-    
-            AcrylicMain = Color3.fromRGB(20, 20, 20),
-            AcrylicBorder = Color3.fromRGB(13, 13, 13),
-            AcrylicGradient = ColorSequence.new(Color3.fromRGB(20, 20, 20), Color3.fromRGB(15, 15, 15)),
-            AcrylicNoise = 0.98,
-    
-            TitleBarLine = Color3.fromRGB(13, 13, 13),
-            Tab = Color3.fromRGB(173, 216, 230),
-    
-            Element = Color3.fromRGB(62, 62, 62),
-            ElementBorder = Color3.fromRGB(18, 18, 18),
-            InElementBorder = Color3.fromRGB(15, 15, 15),
-            ElementTransparency = 0.87,
-    
-            ToggleSlider = Color3.fromRGB(223, 223, 223),
-            ToggleToggled = Color3.fromRGB(40, 40, 40),
-    
-            SliderRail = Color3.fromRGB(173, 216, 230),
-    
-            DropdownFrame = Color3.fromRGB(180, 180, 180),
-            DropdownHolder = Color3.fromRGB(20, 20, 20),
-            DropdownBorder = Color3.fromRGB(15, 15, 15),
-            DropdownOption = Color3.fromRGB(57, 57, 57),
-    
-            Keybind = Color3.fromRGB(20, 20, 20),
-    
-            Input = Color3.fromRGB(20, 20, 20),
-            InputFocused = Color3.fromRGB(20, 10, 30),
-            InputIndicator = Color3.fromRGB(25, 25, 25),
-    
-            Dialog = Color3.fromRGB(24, 24, 24),
-            DialogHolder = Color3.fromRGB(27, 27, 27),
-            DialogHolderLine = Color3.fromRGB(20, 20, 20),
-            DialogButton = Color3.fromRGB(40, 40, 40),
-            DialogButtonBorder = Color3.fromRGB(27, 27, 27),
-            DialogBorder = Color3.fromRGB(172, 172, 172),
-            DialogInput = Color3.fromRGB(35, 35, 35),
-            DialogInputLine = Color3.fromRGB(13, 13, 13),
-    
-            Text = Color3.fromRGB(247, 227, 233),
-            SubText = Color3.fromRGB(170, 170, 170),
-            Hover = Color3.fromRGB(35, 35, 35),
-            HoverChange = 0.04,
-        }
-    end
+	[55] = function()local maui,script,require,getfenv,setfenv=ImportGlobals(55)return {
+		Name = "Rose lllll",
+		Accent = Color3.fromRGB(180, 55, 90),
+
+		AcrylicMain = Color3.fromRGB(40, 40, 40),
+		AcrylicBorder = Color3.fromRGB(130, 90, 110),
+		AcrylicGradient = ColorSequence.new(Color3.fromRGB(190, 60, 135), Color3.fromRGB(165, 50, 70)),
+		AcrylicNoise = 0.92,
+
+		TitleBarLine = Color3.fromRGB(140, 85, 105),
+		Tab = Color3.fromRGB(180, 140, 160),
+
+		Element = Color3.fromRGB(200, 120, 170),
+		ElementBorder = Color3.fromRGB(110, 70, 85),
+		InElementBorder = Color3.fromRGB(120, 90, 90),
+		ElementTransparency = 0.86,
+
+		ToggleSlider = Color3.fromRGB(200, 120, 170),
+		ToggleToggled = Color3.fromRGB(0, 0, 0),
+
+		SliderRail = Color3.fromRGB(200, 120, 170),
+
+		DropdownFrame = Color3.fromRGB(200, 160, 180),
+		DropdownHolder = Color3.fromRGB(120, 50, 75),
+		DropdownBorder = Color3.fromRGB(90, 40, 55),
+		DropdownOption = Color3.fromRGB(200, 120, 170),
+
+		Keybind = Color3.fromRGB(200, 120, 170),
+
+		Input = Color3.fromRGB(200, 120, 170),
+		InputFocused = Color3.fromRGB(20, 10, 30),
+		InputIndicator = Color3.fromRGB(170, 150, 190),
+
+		Dialog = Color3.fromRGB(120, 50, 75),
+		DialogHolder = Color3.fromRGB(95, 40, 60),
+		DialogHolderLine = Color3.fromRGB(90, 35, 55),
+		DialogButton = Color3.fromRGB(120, 50, 75),
+		DialogButtonBorder = Color3.fromRGB(155, 90, 115),
+		DialogBorder = Color3.fromRGB(100, 70, 90),
+		DialogInput = Color3.fromRGB(135, 55, 80),
+		DialogInputLine = Color3.fromRGB(190, 160, 180),
+
+		Text = Color3.fromRGB(240, 240, 240),
+		SubText = Color3.fromRGB(170, 170, 170),
+		Hover = Color3.fromRGB(200, 120, 170),
+		HoverChange = 0.04,
+		}
+	end
 } -- [RefId] = Closure
 
 -- Set up from data
